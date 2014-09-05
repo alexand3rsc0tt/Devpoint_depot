62f3d1ec440f3619a9fb91cc0654d792035d7fdb
alexand3rsc0tt <alex.burton1993@gmail.com>
2014-09-05 13:27:46 -0600

    added style and create new user

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,22 @@
-Rails.application.routes.draw do
+Depot::Application.routes.draw do
+  get 'admin' => 'admin#index'
+  controller :sessions do
+    get  'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
+  end
+
+  get "sessions/create"
+  get "sessions/destroy"
   resources :users
-
   resources :orders
-
   resources :line_items
-
   resources :carts
 
   get "store/index"
   resources :products do
     get :who_bought, on: :member
   end
-
-  resources :products
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
